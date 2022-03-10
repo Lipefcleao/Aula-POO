ed3df561083720eca03cdaf523714ccc51276be0
Felipe Leão <felipefcleao@gmail.com>
2022-03-10 17:03:16 -0300

    Aula 2

--- Caneta.java
+++ Caneta.java
@@ -1,4 +1,32 @@
 
 public class Caneta {
-
+		String modelo;
+		String cor;
+		float ponta;
+		int carga;
+		boolean tampada;
+		
+		void status() {
+			System.out.println("Uma caneta " + this.cor);
+			System.out.println("está tampada? " + this.tampada);
+			System.out.println("Tamanho da ponta: " + this.ponta);
+			System.out.println("Quanta carga: " + this.carga);
+		}
+		void rabiscar() {
+			if(this.tampada == true) {
+				System.out.println("ERRO");
+			}else {
+				System.out.println("rabiscando");
+			}
+		}
+		
+		void tampar() {
+			this.tampada = true;
+		}
+		
+		void destampar() {
+			this.tampada = false;
+		}
+		
+		
 }
